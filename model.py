fix: zoneout w/o scaling corrected and apply by default

model.py
@@ -38,12 +38,19 @@ class QRNN_lm(object):
         self.logits, self.output = self.inference()
         self.loss = self.lm_loss(self.logits, self.words_gtruth)
         self.loss_summary = scalar_summary('loss', self.loss)
+        self.perp_summary = scalar_summary('perplexity', tf.exp(self.loss))
         # set up optimizer
         self.lr = tf.Variable(args.learning_rate, trainable=False)
         self.lr_summary = scalar_summary('lr', self.lr)
         tvars = tf.trainable_variables()
-        grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars),
-                                          args.grad_clip)
+        grads = []
+        for grad in tf.gradients(self.loss, tvars):
+            if grad is not None:
+                grads.append(tf.clip_by_norm(grad, args.grad_clip))
+            else:
+                grads.append(grad)
+        #grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars),
+        #                                  args.grad_clip)
         self.opt = tf.train.GradientDescentOptimizer(self.lr)
         self.train_op = self.opt.apply_gradients(zip(grads, tvars))
 
@@ -99,8 +106,19 @@ class QRNN_lm(object):
             return logits, output
 
     def lm_loss(self, logits, words_gtruth):
-        f_words_gtruth = tf.reshape(words_gtruth,
-                                    [self.batch_size * self.seq_len])
-        loss =  tf.nn.sparse_softmax_cross_entropy_with_logits(logits,
-                                                               f_words_gtruth)
-        return tf.reduce_mean(loss)
+        gtruth_splits = tf.split(1, self.seq_len, words_gtruth)
+        logits_splits = tf.split(1, self.seq_len, tf.reshape(logits,
+                                                             [self.batch_size, 
+                                                              self.seq_len, -1]))
+        #f_words_gtruth = tf.reshape(words_gtruth,
+        #                            [self.batch_size * self.seq_len])
+        loss = 0
+        for idx, (preds, gtruth) in enumerate(zip(logits_splits, gtruth_splits)):
+            preds = tf.squeeze(preds)
+            gtruth = tf.squeeze(gtruth)
+            loss += tf.nn.sparse_softmax_cross_entropy_with_logits(preds,
+                                                                   gtruth)
+        loss = tf.reduce_mean(loss) / self.seq_len
+        #loss =  tf.nn.sparse_softmax_cross_entropy_with_logits(logits,
+        #                                                       f_words_gtruth)
+        return loss

qrnn.py
@@ -1,5 +1,12 @@
 import tensorflow as tf
+import numbers
 from tensorflow.contrib.layers import xavier_initializer
+from tensorflow.python.framework import tensor_shape
+from tensorflow.python.framework import tensor_util
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import random_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.framework import ops
 
 
 def zoneout(x, keep_prob, noise_shape=None, seed=None, name=None):
@@ -27,7 +34,7 @@ def zoneout(x, keep_prob, noise_shape=None, seed=None, name=None):
     Raises:
       ValueError: If `keep_prob` is not in `(0, 1]`.
     """
-    with ops.name_scope(name, "dropout", [x]) as name:
+    with tf.name_scope(name, "dropout", [x]) as name:
         x = ops.convert_to_tensor(x, name="x")
         if isinstance(keep_prob, numbers.Real) and not 0 < keep_prob <= 1:
             raise ValueError("keep_prob must be a scalar tensor or a float in the "
@@ -133,7 +140,7 @@ class QRNN_layer(object):
             self.h = H
             self.last_state = last_C
 
-    def convolution(self, input_, filter_width, out_fmaps, pool_type, zoneout):
+    def convolution(self, input_, filter_width, out_fmaps, pool_type, zoneout_):
         """ Applies 1D convolution along time-dimension (T) assuming input
             tensor of dim (batch_size, T, n) and returns
             (batch_size, T, out_fmaps)
@@ -158,9 +165,9 @@ class QRNN_layer(object):
                 g_a = tf.nn.conv1d(pinput, Wg, stride=1, padding='VALID')
                 g = tf.sigmoid(g_a)
                 if not self.infer and zoneout > 0 and gate_name == 'f':
-                    print('Applying zoneout {} to gate F'.format(zoneout))
+                    print('Applying zoneout {} to gate F'.format(zoneout_))
                     # appy zoneout to F
-                    g = zoneout((1. - g), 1. - zoneout)
+                    g = zoneout((1. - g), 1. - zoneout_)
                     # g = 1. - tf.nn.dropout((1. - g), 1. - zoneout)
                 gates.append(g)
         return z, gates

train_lm.py
@@ -20,9 +20,9 @@ flags.DEFINE_integer("qrnn_size", 640, "Number of qrnn units per layer "
                                        "(Def: 640).")
 flags.DEFINE_integer("qrnn_layers", 2, "Number of qrnn layers (Def: 2). ")
 flags.DEFINE_integer("qrnn_k", 2, "Width of QRNN filter (Def: 2). ")
-flags.DEFINE_integer("emb_dim", 650, "Embedding dimension (Def: 650). ")
+flags.DEFINE_integer("emb_dim", 640, "Embedding dimension (Def: 650). ")
 flags.DEFINE_integer("vocab_size", 10001, "Num words in vocab (Def: 10001). ")
-flags.DEFINE_float("zoneout", 0., "Apply zoneout (dropout) to F gate (Def: 0)")
+flags.DEFINE_float("zoneout", 0.1, "Apply zoneout (dropout) to F gate (Def: 0.1)")
 flags.DEFINE_float("dropout", 0.5, "Apply dropout in hidden layers (Def: 0.5)")
 flags.DEFINE_float("learning_rate", 1., "Beginning learning rate (Def: 1).")
 flags.DEFINE_float("learning_rate_decay", 0.95, "After 6th epoch this "
@@ -109,7 +109,10 @@ def train(lm_model, loader, args):
             if (batch_i + 1) >= batches_per_epoch:
                 break
         return np.mean(tr_loss)
-    with tf.Session() as sess:
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth=True
+    config.allow_soft_placement=True
+    with tf.Session(config=config) as sess:
         try:
             tf.global_variables_initializer().run()
             merged = tf.summary.merge_all()